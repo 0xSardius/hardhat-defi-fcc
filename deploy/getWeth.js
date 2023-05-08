38312ab2db35b36ceee20e3fb9819c7be11cc02f
Sardius <0xsardius@gmail.com>
2023-05-08 19:08:54 -0400

    Adding mainnet fork code

--- deploy/getWeth.js
+++
@@ -1,9 +0,0 @@
-const { getNamedAccounts } = require("hardhat")
-
-async function getWeth() {
-    const { deployer } = await getNamedAccounts()
-    // call the "deposit" function on the weth contract
-    // abi, contract address
-}
-
-modeul.exports = { getWeth }
---
+++ scripts/aaveBorrow.js
@@ -0,0 +1,10 @@
+const { getWeth } = require("./getWeth.js")
+
+async function main() {}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
